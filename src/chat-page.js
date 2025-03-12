cbcfa91b26d81904191636adc76d604d8ba36f73
DESKTOP-ITVR263\Chandler Sihom <csihom@iastate.edu>
2025-03-11 22:03:03 -0500

    outline for chat box

--- src/chat-page.js
+++ src/chat-page.js
@@ -3,13 +3,18 @@ import "./landing-page.css"
 const ChatPage = ({name}) => {
     return (
         <div className="landing-page">
-            <div id="chat-names-holder">
+            <div id="chat-names-holder"> {/* Div for the side bar to change chats */}
                 <p>{name}</p>
             </div>
-            <div id="chat-holder">
-            <p>
-                Chat's holder
-            </p>
+            <div id="chat-holder"> {/* The div for the whole chat box */}
+                <div id="other-person-message" style={{width: "100%"}}> {/* Div for other person's incoming messages */}
+                    <p className="user-messages">Test Message</p> 
+                </div>
+
+                <div id="your-messages" style={{width: "100%"}}>  {/* Div for your own sent messages */}
+                    <p className="user-messages">Test Message</p> 
+                </div>
+                <div id="message-box" aria-placeholder="Type here"></div> {/* Box to input your own message to send */}
             </div>
         </div>
     )
