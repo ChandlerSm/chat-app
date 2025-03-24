f648e7046263f401cd483c99110ebc64fae00152
DESKTOP-ITVR263\Chandler Sihom <csihom@iastate.edu>
2025-03-23 20:15:28 -0500

    Adding backend calls to catch sql data

--- server/db/statements.sql
+++ server/db/statements.sql
@@ -1,6 +1,13 @@
-DROP TABLE chat;
-DROP TABLE chat_participants;
-DROP TABLE message;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS chat;
+DROP TABLE IF EXISTS chat_participants;
+DROP TABLE IF EXISTS message;
+
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS chat (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,6 +30,8 @@ CREATE TABLE IF NOT EXISTS message (
     FOREIGN KEY (chat_id) REFERENCES chat(id) -- This will reference to the chat's current id
 );
 
+INSERT INTO user (username, password) VALUES ('Alice', 'TestPassword');
+
 INSERT INTO chat (name) VALUES ("Test Chat");
 
 INSERT INTO chat_participants (chat_id, name) -- Inserts chat participants into a specified chat by id.
@@ -36,6 +45,8 @@ VALUES
 (1, 'Alice', 'Hello Guys!'),
 (1, 'Bob', 'Hello Alice!'),
 (1, 'John', 'Hello guys');
+
+SELECT * FROM user;
 
 SELECT * FROM chat;
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const http = require('http');
 const socketIo = require('socket.io');
@@ -5,6 +6,7 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
+// Used to connect the websockets
 const io = socketIo(server, {
   cors: {
     origin: 'http://localhost:3001',
@@ -31,6 +33,100 @@ io.on('connection', (socket) => {
         }
     });
 });
+
+// Pulling backend data from SQLite database
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./db/database.db', (err) => {
+  if (err) {
+    console.log("Error connecting to DB");
+  }
+  else {
+    console.log("Connected to db.")
+  }
+});
+
+// Return a list of all messages, mainly for testing
+const getAllMessages = () => {
+db.all("SELECT * FROM message", (err, rows) => {
+  if (err) {
+    console.log("Could not query messages")
+  }
+  else {
+    console.log("All messages:", rows);
+  }
+});
+}
+
+// Return a list of all chats, mainly for testing
+const getAllChats = () => {
+  db.all("SELECT * FROM chat", (err, rows) => {
+    if (err) {
+      console.log("Could not query chat")
+    }
+    else {
+      console.log("All chats:", rows);
+    }
+  });
+}
+
+// Return a list of all partipents in each chat, mainly for testing
+const getAllParticipants = () => {
+  db.all("SELECT * FROM chat_participants", (err, rows) => {
+    if (err) {
+      console.log("Could not query participants")
+    }
+    else {
+      console.log("All participants:", rows);
+    }
+  });
+}
+
+// Create a chat
+const createChat = (chat_name) => {
+  db.exec(`INSERT INTO chat (name) VALUES ('${chat_name}'); 
+  INSERT INTO chat_participants (chat_id, name) VALUES (2, 'Gray');`, (err) => {
+    if (err) {
+      console.log("Could not insert chat");
+    }
+    else {
+      console.log("Inserted new chat name");
+    }
+  })
+  }
+
+// Add a user into the chat
+const addParticipants = (chat_id, name) => {
+  if (name.length === 0) {
+    console.log("No users in the list");
+  } else {
+    for (let i = 0; i < name.length; i++) {
+      db.exec(`INSERT INTO chat_participants (chat_id, name) VALUES ('${chat_id}', '${name[i]}');`, (err) => {
+        if (err) {
+          console.log("Could not insert participant: ", err);
+        } else {
+          console.log(`Successfully added participant ${name[i]} at ${chat_id}`);
+        }
+      });
+    }
+  }
+};
+
+
+createChat("test chat 3");
+addParticipants(2, ["Red", "Blue"]);
+getAllMessages();
+getAllChats();
+getAllParticipants();
+
+// Close the database
+// db.close((err) => {
+//   if (err) {
+//       console.error('Error closing database connection:', err.message);
+//   } else {
+//       console.log('Closed the SQLite database connection.');
+//   }
+// });
 
 const PORT = 3000;
 server.listen(PORT, () => {
